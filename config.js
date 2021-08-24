646d8648e830270c56fd869f99dbd832a5a29238
Dynamiccreeper <74767888+Dynamiccreeper@users.noreply.github.com>
2021-08-24 17:22:43 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+
+  export const firebaseConfig = {
+    apiKey: "AIzaSyBuIxpcBhNxU18AiHFgzhVSSiVlC3xNi0Q",
+    authDomain: "spectagram-app-f49f5.firebaseapp.com",
+    projectId: "spectagram-app-f49f5",
+    storageBucket: "spectagram-app-f49f5.appspot.com",
+    messagingSenderId: "360946575310",
+    appId: "1:360946575310:web:0c45e392f625b3a9a16d9f"
+  };
